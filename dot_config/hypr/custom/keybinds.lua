561937c4f7dc1052c08cb7891d1615f0460f7fe8
seekode <nicolas@seekode.dev>
2026-08-23 17:34:27 +0200

    feature: add shortcut to make à screenshot and save it automatically

--- dot_config/hypr/custom/keybinds.lua
+++ dot_config/hypr/custom/keybinds.lua
@@ -8,3 +8,5 @@ for i = 1, 10 do
         hl.dispatch(hl.dsp.window.move({ workspace = workspace_in_group(i), follow = true }))
     end, { description = "Window: Send to workspace " .. i })
 end
+
+hl.bind("SUPER + CTRL + SHIFT + S", hl.dsp.exec_cmd("grim -g \"$(slurp)\" - | swappy -f -"), { description = "Utilities: Screen snip >> Swappy" })
